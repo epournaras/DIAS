161943b32f0c5e7b1e2b51338e1678edafa0b402
loblum <blum@id.ethz.ch>
2015-07-20 15:27:17 +0200

    distributed (brutus) execution

---
+++ run/run_distributed.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+nodes=20
+runtime=650
+folder=dump/distr_${nodes}n_${runtime}s/
+
+#args= folder, ID, Port (port0=auto)
+java -Dvar=live0 -cp ../lib/*:../build/classes/ protocols.DIASLiveExperiment $folder 0 5555 &
+
+i=1
+while [ $i -le $nodes ]; do
+    bsub -R rusage[mem=2014] java -Xmx1G -Dvar=distr$i -cp ../lib/*:../build/classes/ protocols.DIASLiveExperiment $folder $i 0 
+    i=$((i+1))
+done      
+
+echo "WAITING $runtime s (STOP WITH ENTER)"
+read -t $runtime
+
+kill %1
+bkill $(bjobs -w | grep "protocols.DIASLiveExperiment" | awk '{print $1}')
