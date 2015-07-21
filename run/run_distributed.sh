d2abfec32bb22f3db18f4ff6931f821ec312c23a
Rok Roskar <roskarr@ethz.ch>
2015-07-21 13:56:26 +0200

    fixed communication issues with running on cluster

--- run/run_distributed.sh
+++ run/run_distributed.sh
@@ -3,13 +3,16 @@
 nodes=20
 runtime=650
 folder=dump/distr_${nodes}n_${runtime}s/
+ip=$(grep $(hostname) /etc/hosts | awk '{print $1}')
+echo $ip
+sed -i "s/peerZeroIP=.*/peerZeroIP=$ip/" conf/protopeer.conf
 
 #args= folder, ID, Port (port0=auto)
-java -Dvar=live0 -cp ../lib/*:../build/classes/ protocols.DIASLiveExperiment $folder 0 5555 &
+java -Dvar=distr0 -cp ../lib/*:../build/classes/:../lib/unused/ProtoPeer.jar protocols.DIASLiveExperiment $folder 0 5555 &
 
 i=1
 while [ $i -le $nodes ]; do
-    bsub -R rusage[mem=2014] java -Xmx1G -Dvar=distr$i -cp ../lib/*:../build/classes/ protocols.DIASLiveExperiment $folder $i 0 
+    bsub -R rusage[mem=2014] java -Xmx1G -Dvar=distr$i -cp ../lib/*:../build/classes/:../lib/unused/ProtoPeer.jar protocols.DIASLiveExperiment $folder $i 0 
     i=$((i+1))
 done      
 
