94bd5e26c946120dbbd9773e6404ed34e2390153
epournaras <evangelospournaras@gess-rz-dock-1-065.ethz.ch>
2016-02-17 10:58:21 +0100

    Fixed paths in run_live script

--- run_live.sh
+++ run_live.sh
@@ -7,12 +7,12 @@ folder=dump/live_${nodes}nodes_${runtime}s/
 #args= folder, ID, Port (port0=auto)
 rm -r $folder
 mkdir -p $folder
-java -Dvar=$(basename $folder)_0 -cp ../lib/*:../build/classes/ protocols.DIASLiveExperiment $folder 0 5555 &
+java -Dvar=$(basename $folder)_0 -cp ./lib/*:./build/classes/ protocols.DIASLiveExperiment $folder 0 5555 &
 sleep 1
 
 i=1
 while [ $i -lt $nodes ]; do
-    java -Dvar=$(basename $folder)_$i -cp ../lib/*:../build/classes/ protocols.DIASLiveExperiment $folder $i 0 &
+    java -Dvar=$(basename $folder)_$i -cp ./lib/*:./build/classes/ protocols.DIASLiveExperiment $folder $i 0 &
     i=$((i+1))
 done      
 sleep 3
@@ -28,5 +28,5 @@ echo ENDING
 pkill java
 sleep 1s
 
-java -cp ../lib/*:../build/classes/ protocols.DIASLogReplayer $folder | tail -n+4 > summaries/$(basename $folder).dat 
+java -cp ./lib/*:./build/classes/ protocols.DIASLogReplayer $folder | tail -n+4 > summaries/$(basename $folder).dat 
 python plot.py summaries/$(basename $folder).dat
